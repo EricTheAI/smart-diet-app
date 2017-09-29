abe63d7406e9b5724f4ce367de646881225cbb5b
Hugo Chau <openh79q83@gmail.com>
2017-09-29 14:11:28 +1000

    demo echart

--- src/pages/today/today.ts
+++ src/pages/today/today.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { FoodConfirmPage } from '../food-confirm/food-confirm';
-import { TodayPage } from '../today/today';
 import { ScanPage } from '../scan/scan';
-import { FoodConfirmPage } from '../food-confirm/food-confirm';
 import { ManuallyAddFoodPage } from '../manually-add-food/manually-add-food';
 import { CameraPage } from '../camera/camera';
-
+declare var echarts;
 @Component({
   selector: 'page-today',
   templateUrl: 'today.html'
@@ -15,6 +13,32 @@ export class TodayPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   constructor(public navCtrl: NavController) {
+  }
+  ionViewDidLoad(){
+    var myChart = echarts.init(document.getElementById("today-container2"));
+    
+            // 指定图表的配置项和数据
+            var option = {
+                title: {
+                    text: 'ECharts 入门示例'
+                },
+                tooltip: {},
+                legend: {
+                    data:['销量']
+                },
+                xAxis: {
+                    data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            };
+    
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
   }
   goToFoodConfirm(params){
     if (!params) params = {};
@@ -25,9 +49,6 @@ export class TodayPage {
   }goToScan(params){
     if (!params) params = {};
     this.navCtrl.push(ScanPage);
-  }goToFoodConfirm(params){
-    if (!params) params = {};
-    this.navCtrl.push(FoodConfirmPage);
   }goToManuallyAddFood(params){
     if (!params) params = {};
     this.navCtrl.push(ManuallyAddFoodPage);
