87835e6cc981da0ef6b663bdc10a4ed46f57bc43
Yi Song (Ethan) <ethaninmel@gmail.com>
2017-09-29 16:09:18 +1000

    server code setup

---
+++ server/config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+config.host = process.env.HOST || "https://smartbite.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "UjpHlffsn4w0UBdnlZBCHqmcopvDOaCMyTOfwgwe98vk29NYsgUrHMD0FTRgLS5kIWyorNbYowHk9dnZ3EfFvA==";
+config.databaseId = "ToDoList";
+config.collectionId = "Items";
+
+module.exports = config;
